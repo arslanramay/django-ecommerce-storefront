28966640d17ed54f49e1c53c3821007fef42b677
Arslan Ramay <arslanramay104@gmail.com>
2025-03-06 19:46:27 +0400

    Updated Models

--- likes/models.py
+++ likes/models.py
@@ -1,16 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.auth.models import User
 
-class Tag(models.Model):
-    name = models.CharField(max_length=255)
-
-class TaggedItem(models.Model):
-    # What Tag is applied to what item/object
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-
-    # What item/object is tagged
+# Trach What User Likes
+class LikedItem(models.Model):
+    # What user likes what object
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
