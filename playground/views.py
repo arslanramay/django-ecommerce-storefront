c6d1d87bc0016d848ba2d86d9c342c93be42a2a5
Arslan Ramay <arslanramay104@gmail.com>
2025-03-08 15:52:45 +0400

    Minor changes

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
-# Create your views here.
-def calculate():
-    x = 1
-    y = 2
-    return x + y
 
 def say_hello(request):
     # return HttpResponse('Hello, Django!')
     # return render(request, 'hello.html')
 
-    x = calculate()
+    # Simple Try/Except block
+    try:
+        product = Product.objects.get(id=-2)
+    except ObjectDoesNotExist:
+        pass
+
+    # Get all products
+    # query_set = Product.objects.all()
+    # for product in query_set:
+    #     print(product)
+
+    # Return HTML page with name
     return render(request, 'hello.html', {'name': 'Arslan'})
