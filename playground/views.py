3fe5871f9936d2f504fad7ff2c453ee64526f203
Arslan Ramay <arslanramay104@gmail.com>
2025-03-09 00:01:21 +0400

    Implemented ORM Filters to Retrieve data from the Products table

--- playground/views.py
+++ playground/views.py
@@ -20,8 +20,16 @@ def say_hello(request):
     #     print(product)
 
     # Filters
-    query_set = Product.objects.filter(unit_price__range=(40, 60))
+    # query_set = Product.objects.filter(unit_price__range=(40, 60))
+    # query_set = Product.objects.filter(collection__id=6)
+    # query_set = Product.objects.filter(title__icontains='coffee')
+    # query_set = Product.objects.filter(title__startswith='Coffee')
+    # query_set = Product.objects.filter(last_update__year='2021')
+    query_set = Product.objects.filter(last_update__month='6')
     # print(query_set)
+
+    # Filters
+    # Products: inventory < 10 AND price < 20
 
     # Render the template with Products
     return render(request, 'hello.html', {'name': 'Arslan', 'products': list(query_set)})
