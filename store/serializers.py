14a2e3181342131f6cb3fdd6924971b59a2301fe
Arslan Ramay <arslanramay104@gmail.com>
2025-03-16 14:58:49 +0400

    Minor changes

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,13 @@
+from decimal import Decimal
 from rest_framework import serializers
+from .models import Product
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
+
+    def get_price_with_tax(self, product : Product):
+        return product.unit_price * Decimal('1.1')
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,14 +9,26 @@ from .serializers import ProductSerializer
 # Define API Views
 @api_view()
 def product_list(request):
-    return Response('sProduct List Page')
+    # products = quertyset
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True) # many=True for iterating on multiple objects
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    try:
-        product = Product.objects.get(pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-        # return Response(f'Product Detail Page: {id}')
-    except Product.DoesNotExist:
-        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+
+
+# Without get_obj_or_404
+# def product_detail(request, id):
+#     try:
+#         product = Product.objects.get(pk=id)
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)
+#         # return Response(f'Product Detail Page: {id}')
+#     except Product.DoesNotExist:
+#         return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
