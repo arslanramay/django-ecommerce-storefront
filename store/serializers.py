9ec9f4ea70edb43f466c862089e6d47aa3c56341
Arslan Ramay <arslanramay104@gmail.com>
2025-03-17 16:54:38 +0400

    (feat): Implement Model Serializer to reduce Code Duplication in Serializers class

--- store/serializers.py
+++ store/serializers.py
@@ -3,18 +3,25 @@ from rest_framework import serializers
 from decimal import Decimal
 from .models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
     # featured_product = serializers.PrimaryKeyRelatedField(
     #     read_only=True
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # description = serializers.CharField()
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
+
     # Fetch related Collection object
     # 1. Primary key:: This will return Collection IDs only
     # collection = serializers.PrimaryKeyRelatedField(
@@ -24,7 +31,6 @@ class ProductSerializer(serializers.Serializer):
     # collection = serializers.StringRelatedField()
     # 3. Nested Object:: This will return Nested Collection object (1 extra SQL query)
     # collection = CollectionSerializer()
-
     # 4. Hyperlink:: Return Hyper Links
     collection = serializers.HyperlinkedRelatedField(
         queryset=Collection.objects.all(),
