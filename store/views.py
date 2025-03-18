bf021b8e7ab14fe695768368d8d00b233ba64fc2
Arslan Ramay <arslanramay104@gmail.com>
2025-03-18 17:45:24 +0400

    (feat): Implemented Delete Object logic to delete Products from the Database

--- store/views.py
+++ store/views.py
@@ -38,6 +38,15 @@ def product_detail(request, id):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response(
+                {
+                    'error': 'Product cannot be deleted because it is associated with an order item.'
+                },
+                status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view()
 def collection_detail(request, pk):
