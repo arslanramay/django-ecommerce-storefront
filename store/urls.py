ce5fb5962e87fe5cdd18a6ddeace681ca208164a
Arslan Ramay <arslanramay104@gmail.com>
2025-03-25 15:20:20 +0400

    (feat): Implemented Destroy methods for Product and Collection ViewSets

--- store/urls.py
+++ store/urls.py
@@ -1,21 +1,27 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
+from pprint import pprint
+
+# Router URLs
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+# pprint(router.urls)
 
 # URLConf
-urlpatterns = [
-    # Function based views
-    # path('products/', views.product_list),
-    # path('products/<int:id>/', views.product_detail),
+urlpatterns = router.urls
 
-    # Class based views
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
+# urlpatterns = [
+# #     # Function based views
+# #     # path('products/', views.product_list),
+# #     # path('products/<int:id>/', views.product_detail),
+# #     # path('collections/', views.collection_list),
+# #     # path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
 
-    # Function based views
-    # path('collections/', views.collection_list),
-    # path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
-
-    # Class based views
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view()),
-]
+# #     # Class based views
+# #     # path('products/', views.ProductList.as_view()),
+# #     # path('products/<int:pk>/', views.ProductDetail.as_view()),
+# #     # path('collections/', views.CollectionList.as_view()),
+# #     # path('collections/<int:pk>/', views.CollectionDetail.as_view()),
+# ]
--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 from .serializers import ProductSerializer, CollectionSerializer
 
 # ======================
@@ -23,31 +23,50 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response(
                 {
                     'error': 'Product cannot be deleted because it is associated with an order item.'
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+    # Use he above destry() method to delete the product in ViewSet method
+    # def delete(self, request, pk):
+    #     product = get_object_or_404(Product, pk=pk)
+    #     if product.orderitems.count() > 0:
+    #         return Response(
+    #             {
+    #                 'error': 'Product cannot be deleted because it is associated with an order item.'
+    #             },
+    #             status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response(
                 {
                     'error': 'Collection cannot be deleted because it includes one or more products.'
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+    # def delete(self, request, pk):
+    #     collection = get_object_or_404(Collection, pk=pk)
+    #     if collection.products.count() > 0:
+    #         return Response(
+    #             {
+    #                 'error': 'Collection cannot be deleted because it includes one or more products.'
+    #             },
+    #             status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    #     collection.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # ========================================================================================
